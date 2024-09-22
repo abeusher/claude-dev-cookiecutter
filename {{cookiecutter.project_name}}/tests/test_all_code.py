f59a8dd2bd115bf5f1e5423a6b5f057f54ffdf8b
Abe <abe@blackcape.io>
2024-09-21 21:16:03 -0400

    update

---
+++ {{cookiecutter.project_name}}/tests/test_all_code.py
@@ -0,0 +1,12 @@
+import unittest
+import os
+import csv
+from unittest.mock import patch, mock_open
+import sys
+
+# Add the parent directory to the Python path to import the modules
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# import python modules to be tested here
+
+# create tests here
